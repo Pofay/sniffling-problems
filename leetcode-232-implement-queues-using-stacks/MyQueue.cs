ce7ac15a7df2517ed483a5af31cf4c000481598c
Gian Carlo I. Gilos <eredar102@gmail.com>
2024-02-01 19:47:19 +0800

    chore: Initialized testing framework and is now running properly

---
+++ leetcode-232-implement-queues-using-stacks/MyQueue.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetcodePlayground.Basics
+{
+    // Problem statement at https://leetcode.com/problems/implement-queue-using-stacks/
+
+    public class MyQueue
+    {
+        public MyQueue()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+
+        }
+
+        public int Pop()
+        {
+            return 0;
+        }
+
+        public int Peek()
+        {
+            return 0;
+        }
+
+        public bool Empty()
+        {
+            return true;
+        }
+    }
+}
---
+++ leetcode-232-implement-queues-using-stacks/MyQueueTest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetcodePlayground.Basics
+{
+    public class MyQueueTest
+    {
+        [Fact]
+        public void SmokeTest()
+        {
+            var sut = new MyQueue();
+
+            Assert.True(false);
+        }
+    }
+}
