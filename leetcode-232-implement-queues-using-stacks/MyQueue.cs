8ff8313b1f67e96e2bcc78093789533405669e40
Gian Carlo I. Gilos <eredar102@gmail.com>
2024-02-06 09:27:53 +0800

    cheated: relied on previous submission to solve current problem

--- leetcode-232-implement-queues-using-stacks/MyQueue.cs
+++ leetcode-232-implement-queues-using-stacks/MyQueue.cs
@@ -9,29 +9,56 @@ namespace LeetcodePlayground.Basics
 
     public class MyQueue
     {
+        private Stack<int> stack;
+
         public MyQueue()
         {
-
+            stack = new();
         }
 
         public void Push(int x)
         {
+            if (Empty())
+            {
+                stack.Push(x);
+                return;
+            }
 
+            var intermediate = new Stack<int>();
+            var count = stack.Count;
+
+            for (var i = 0; i < count; i++)
+            {
+                var value = stack.Pop();
+                intermediate.Push(value);
+            }
+            stack.Push(x);
+            var intermediateCount = intermediate.Count;
+            for (var i = 0; i < intermediateCount; i++)
+            {
+                var value = intermediate.Pop();
+                stack.Push(value);
+            }
         }
 
         public int Pop()
         {
-            return 0;
+            return stack.Pop();
         }
 
         public int Peek()
         {
-            return 0;
+            if (Empty())
+            {
+                return 0;
+            }
+
+            return stack.Peek();
         }
 
         public bool Empty()
         {
-            return true;
+            return stack.Count == 0;
         }
     }
 }
--- leetcode-232-implement-queues-using-stacks/MyQueueTest.cs
+++ leetcode-232-implement-queues-using-stacks/MyQueueTest.cs
@@ -9,11 +9,101 @@ namespace LeetcodePlayground.Basics
     public class MyQueueTest
     {
         [Fact]
-        public void SmokeTest()
+        public void QueueIsEmptyOnCreation()
         {
             var sut = new MyQueue();
 
-            Assert.True(false);
+            Assert.True(sut.Empty());
+        }
+
+        [Fact]
+        public void QueueIsNotEmptyOnceElementIsPushed()
+        {
+            var sut = new MyQueue();
+
+            sut.Push(1);
+
+            Assert.False(sut.Empty());
+        }
+
+        [Fact]
+        public void QueueShouldReturn0WhenPeekingOnCreation()
+        {
+            var sut = new MyQueue();
+            var expected = 0;
+
+            var actual = sut.Peek();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void QueueIsEmptyAfterPushAndPop()
+        {
+            var sut = new MyQueue();
+
+            sut.Push(1);
+            sut.Pop();
+
+            Assert.True(sut.Empty());
+        }
+
+        [Fact]
+        public void QueueShouldReturnFirstElementAfterPushAndPeek()
+        {
+            var sut = new MyQueue();
+            var expected = 1;
+
+            sut.Push(1);
+            var actual = sut.Peek();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void QueueShouldReturnFirstElementAfter2PushesAndPeek()
+        {
+            var sut = new MyQueue();
+            var expected = 1;
+
+            sut.Push(1);
+            sut.Push(2);
+            var actual = sut.Peek();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LeetcodeValues()
+        {
+            var sut = new MyQueue();
+
+            sut.Push(1);
+            sut.Push(2);
+            Assert.Equal(1, sut.Peek());
+            sut.Pop();
+            Assert.Equal(2, sut.Peek());
+            Assert.False(sut.Empty());
+        }
+
+        [Fact]
+        public void LeetCodeValues2()
+        {
+            // ["MyQueue","push","push","push","push","pop","push","pop","pop","pop","pop"]
+            // [[],[1],[2],[3],[4],[],[5],[],[],[],[]]
+
+            var sut = new MyQueue();
+
+            sut.Push(1);
+            sut.Push(2);
+            sut.Push(3);
+            sut.Push(4);
+            Assert.Equal(1, sut.Pop());
+            sut.Push(5);
+            Assert.Equal(2, sut.Pop());
+            Assert.Equal(3, sut.Pop());
+            Assert.Equal(4, sut.Pop());
+            Assert.Equal(5, sut.Pop());
         }
     }
 }
