76678c6809b64db0ffe1e98fd6d0a1bacb6e69ac
Gian Carlo I. Gilos <eredar102@gmail.com>
2024-02-07 08:52:23 +0800

    solved: Leetcode 225 has been solved although using 2 Queues instead of just 1

--- leetcode-225-implement-stacks-using-queues/MyStack.cs
+++ leetcode-225-implement-stacks-using-queues/MyStack.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,31 +10,51 @@ namespace LeetcodePlayground.Basics
 
     public class MyStack
     {
+        private Queue<int> queue;
+
         public MyStack()
         {
-
+            queue = new();
         }
 
         public void Push(int x)
         {
+            if (Empty())
+            {
+                queue.Enqueue(x);
+                return;
+            }
 
+            queue.Enqueue(x);
+            var temp = new Queue<int>();
+            var mainCount = queue.Count - 1;
+            for (var i = 0; i < mainCount; i++)
+            {
+                var value = queue.Dequeue();
+                temp.Enqueue(value);
+            }
+            var tempCount = temp.Count;
+            for (var i = 0; i < tempCount; i++)
+            {
+                var value = temp.Dequeue();
+                queue.Enqueue(value);
+            }
         }
 
         public int Pop()
         {
-            return 0;
-
+            return queue.Dequeue();
         }
 
         public int Top()
         {
-            return 0;
+            return queue.Peek();
 
         }
 
         public bool Empty()
         {
-            return true;
+            return queue.Count == 0;
         }
     }
 }
--- leetcode-225-implement-stacks-using-queues/MyStackTest.cs
+++ leetcode-225-implement-stacks-using-queues/MyStackTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using Xunit;
 
 namespace LeetcodePlayground.Basics
@@ -14,6 +15,57 @@ namespace LeetcodePlayground.Basics
             var sut = new MyStack();
 
             Assert.True(sut.Empty());
+        }
+
+        [Fact]
+        public void StackShouldNotBeEmptyOnPush()
+        {
+            var sut = new MyStack();
+
+            sut.Push(2);
+
+            Assert.False(sut.Empty());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void StackShouldReturnTopMostValue(int value)
+        {
+            // Given
+            var sut = new MyStack();
+
+            // When
+            sut.Push(value);
+            var actual = sut.Pop();
+
+            // Then
+            Assert.Equal(value, actual);
+        }
+
+        [Fact]
+        public void StackShouldReturnTopMostValueFor3Pushes()
+        {
+            var sut = new MyStack();
+
+            sut.Push(1);
+            sut.Push(2);
+            sut.Push(3);
+            var actual = sut.Top();
+
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void LeetcodeTest1()
+        {
+            var sut = new MyStack();
+
+            sut.Push(1);
+            sut.Push(2);
+            Assert.Equal(2, sut.Top());
+            Assert.Equal(2, sut.Pop());
+            Assert.False(sut.Empty());
         }
     }
 }
